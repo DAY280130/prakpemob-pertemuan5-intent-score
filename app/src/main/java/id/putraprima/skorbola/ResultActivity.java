7eab2e83daaceff93c226701175649033b0910be
DAY280130 <dimas.280130@gmail.com>
2022-11-13 21:04:30 +0700

    DONE

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -2,13 +2,24 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView tv_result;
+    private Intent result;
+    private String winner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        result = getIntent();
+        winner = result.getStringExtra("winner");
+        tv_result = findViewById(R.id.tv_result);
+        tv_result.setText("Pemenangnya adalah\n" + winner);
     }
 }
